903a53f34bd58bc7ca17b3fea2a2b27b656fae67
goodgamegoodgumaempire-arch <goodgamegoodgumaempire@gmail.com>
2025-09-17 03:20:09 +0300

    Add files via upload

---
+++ vercel.js
@@ -0,0 +1,15 @@
+{
+  "rewrites": [
+    {
+      "source": "/",
+      "destination": "/B-Zone-Politie/index.html"
+    }
+  ],
+  "redirects": [
+    {
+      "source": "/(.*)",
+      "destination": "/B-Zone-Politie/index.html",
+      "permanent": true
+    }
+  ]
+}
